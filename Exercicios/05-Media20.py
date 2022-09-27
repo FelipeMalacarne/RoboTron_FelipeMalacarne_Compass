4d181e58a2918673ada17251457e3d74c1207746
FelipeMalacarne <felipemalacarne012@gmail.com>
2022-09-27 16:56:26 -0300

    Ajustes Exercicios 5 e 10

--- Exercicios/05-Media20.py
+++ Exercicios/05-Media20.py
@@ -1,35 +1,19 @@
 '''
 Construa um programa que recebe 20 valores para X e no final apresenta a média aritmética dos valores pares digitados
 '''
-
-#inicializa lista
-numList = []
-
-#Função pega apenas números pares e monta a lista
-def getNumList(maxRange):
-    for i in range(0, maxRange):
-        print(f"Informe o número de indice: {i}")
-        num = int(input())
-        if isEven(num):
-            numList.append(num)
-        else:
-            continue
-
 def isEven(n):
     return n % 2 == 0
 
-#calcula a média dado a lista como parametro
-def calcularMedia(list):
-    soma = 0
-    for i in range(0, len(list)):
-        soma += list[i]
-    return soma/len(list)
-
 def main():
-    #Requisita 20 numeros, monta a lista com os pares
-    getNumList(20)
-    #Calcula a média da lista com os pares
-    media = calcularMedia(numList)
+    soma = 0
+    counter = 0
+
+    for i in range(0,20):
+        num = int(input(f"Informe o número de indice {i}: "))
+        if isEven(num):
+            soma += num
+            counter += 1
+    media = soma/counter
 
     print(f"A média aritmética da lista de números é: {media}")
 
--- Exercicios/10-frutas.py
+++ Exercicios/10-frutas.py
@@ -1,3 +1,17 @@
 '''
 Crie um programa que recebe uma lista com três frutas e compare com uma lista com os valores ["maça", "banana", "pera"]
+##Checar se as duas listas são exatamente iguais.
 '''
+def main():
+    listaFixa = ["maça", "banana", "pera"]
+    matchCounter = 0
+    for i in range(0,3):
+        aux = input(f"Informe a {i+1} fruta : ")
+        if aux == listaFixa[i]:
+            matchCounter += 1
+   
+    if matchCounter == 3:
+        print("As duas listas são iguais ")
+    else:
+        print("As listas não são iguais ")
+main()
